bc43a3f01963274da69d1f2e3ab068a3c1e450aa
yoraish <yorai.shaoul@gmail.com>
2023-09-02 18:15:56 -0400

    feat(cbs, ecbs): accept low level planner heuristic.

--- src/planners/multi_agent/cbs.cpp
+++ src/planners/multi_agent/cbs.cpp
@@ -59,6 +59,8 @@ void ims::CBS::initializePlanner(std::vector<std::shared_ptr<ConstrainedActionSp
 
     // Create all the low-level planners.
     for (size_t i{0}; i < starts.size(); ++i) {
+        ims::wAStarParams wastar_params_(params_.low_level_heuristic_ptrs[i], params_.weight_low_level_heuristic);
+        
         agent_planner_ptrs_.push_back(std::make_shared<ims::wAStar>(wastar_params_));
     }
 
