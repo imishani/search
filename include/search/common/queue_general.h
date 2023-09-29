0ab8a1041ef836e473de3d4a840b44b2c6228bb2
Rishi-V <rishiveerapaneni@gmail.com>
2023-09-29 15:57:31 -0700

    More comments

--- include/search/common/queue_general.h
+++ include/search/common/queue_general.h
@@ -5,11 +5,19 @@
 
 namespace ims {
 
-/// @brief AbstractQueue which supports basic queue functionality for search
-/// @tparam T can be any arbitrary type
+/// @brief Require LowerBound function for elements.
+struct LowerBoundInterface {
+    virtual double getLowerBound() const = 0;
+};
+
+/// @brief AbstractQueue which supports basic queue functionality for search.
+/// @tparam T must support LowerBoundInterface for getLowerBound() functionality.
 template <class T>
 class AbstractQueue {
 public:
+    static_assert(std::is_base_of<ims::LowerBoundInterface, T>::value,
+                  "T must inherit from ims::LowerBoundInterface");
+
     virtual T* min() const = 0;
     virtual void pop() = 0;
     virtual void push(T* e) = 0;
@@ -17,20 +25,24 @@ public:
     virtual bool empty() const = 0;
     virtual size_t size() const = 0;
 
-    /// @brief Updates the queue to only consider elements with lower bound
-    /// @param lower_bound 
-    /// @note This is mainly required for focal queues
-    virtual void updateWithBound(double lower_bound) = 0;
+    /// @brief Updates the queue to only consider elements that satisfy the lower bound.
+    /// @param lower_bound_threshold is the absolute value, not a suboptimality factor, queue
+    /// should then only pop() elements that satisfy this bound.
+    /// @note This is mainly required for focal queues.
+    virtual void updateWithBound(double lower_bound_threshold) = 0;
 
-    /// @brief Returns lower bound value of the queue
+    /// @brief Returns the lower bound value of the queue (e.g. min->getLowerBound()).
+    /// In papers this is typically the min f-value of the anchor queue.
     /// @return 
-    /// @note This is supported by some queues and not others
+    /// @note This is supported by some queues and not others.
     virtual double getLowerBound() const = 0;
 };
 
-/// @brief Basic queue which returns min element based on comparator
-/// @tparam T
-/// @tparam CompareMain which returns which element is smaller
+/// @brief Basic queue which returns min element based on comparator.
+/// @tparam T element type which must support LowerBoundInterface.
+/// @tparam CompareMain which returns which element is smaller.
+/// @note CompareMain should sort by lower bound!
+/// TODO: MAKE SURE THIS IS THE CASE! Might want to have CompareMain just for tie-breaking?
 template <class T, class CompareMain>
 class SimpleQueue : public AbstractQueue<T> {
 private:
@@ -47,23 +59,21 @@ public:
     virtual bool empty() const override;
     virtual size_t size() const override;
 
-    virtual void updateWithBound(double lower_bound) override;
+    /// @brief Updates the queue to only consider elements that satisfy the lower bound.
+    /// @param lower_bound_threshold 
+    /// @note NOT SUPPORTED for SimpleQueue as queue contains all elements regardless of bound.
+    virtual void updateWithBound(double lower_bound_threshold) override;
 
-    /// @brief Supported
+    /// @brief Supported, returns the lower bound of the min element.
     /// @return 
     virtual double getLowerBound() const override;
 };
 
-/// @brief Require LowerBound function for FocalQueue elements
-struct LowerBoundInterface {
-    virtual double getLowerBound() const = 0;
-};
-
 /// @brief FocalQueue which uses both comparators and returns min element 
-//         satisfying lower bound
-/// @tparam T element type which must support LowerBoundInterface
-/// @tparam CompareMain used for anchor queue
-/// @tparam CompareFocal used for focal queue
+//         satisfying lower bound.
+/// @tparam T element type which must support LowerBoundInterface.
+/// @tparam CompareMain used for anchor queue.
+/// @tparam CompareFocal used for focal queue.
 template <class T, class CompareMain, class CompareFocal>
 class FocalQueue : public AbstractQueue<T> {
 private:
@@ -71,8 +81,6 @@ private:
     smpl::IntrusiveHeapWrapper<T, CompareFocal> m_focal;
 
 public:
-    static_assert(std::is_base_of<ims::LowerBoundInterface, T>::value,
-                  "T must inherit from ims::LowerBoundInterface");
     virtual T* min() const override;
     virtual void pop() override;
     virtual void push(T* e) override;
@@ -80,19 +88,21 @@ public:
     virtual bool empty() const override;
     virtual size_t size() const override;
 
-    /// @brief Updates queue to add more elements to focal list
-    /// @param lower_bound 
-    virtual void updateWithBound(double lower_bound) override;
+    /// @brief Updates queue to add more elements to focal list which satisfy lower bound threshold.
+    /// @param lower_bound_threshold is the absolute value, not a suboptimality factor, queue
+    /// should then only pop() elements that satisfy this bound.
+    virtual void updateWithBound(double lower_bound_threshold) override;
 
-    /// @brief Not supported as FocalQueue does not keep track of this
+    /// @brief Not supported as FocalQueue does not keep track of this (as the focal queue is sorted by
+    /// other values).
     /// @return 
     virtual double getLowerBound() const override;
 };
 
-/// @brief Wrapper class that enables focal queue expansion and keeps track of lower bound
-/// @tparam T element type which must support LowerBoundInterface
-/// @tparam CompareMain used for anchor queue
-/// @tparam CompareFocal used for focal queue
+/// @brief Wrapper class that enables focal queue expansion and keeps track of lower bound.
+/// @tparam T element type which must support LowerBoundInterface.
+/// @tparam CompareMain used for anchor queue.
+/// @tparam CompareFocal used for focal queue.
 template <class T, class CompareMain, class CompareFocal>
 class FocalAndAnchorQueueWrapper : public AbstractQueue<T> {
 private:
@@ -107,12 +117,14 @@ public:
     virtual bool empty() const override;
     virtual size_t size() const override;
 
-    /// @brief Updates focal queue to add more elements satisfying lower bound
-    /// @param lower_bound 
-    virtual void updateWithBound(double lower_bound) override;
+    /// @brief Updates focal queue to add more elements satisfying lower bound threshold.
+    /// @param lower_bound_threshold is the absolute value, not a suboptimality factor, queue
+    /// should then only pop() elements that satisfy this bound.
+    virtual void updateWithBound(double lower_bound_threshold) override;
 
-    /// @brief Supported now as contains anchor queue
+    /// @brief Returns the lower bound of all elements in the Queue.
     /// @return 
+    /// @note Supported now as we internally have a SimpleQueue which keeps track of the lower bound.
     virtual double getLowerBound() const override;
 };
 
