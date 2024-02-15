1fdf38d65125ea2953162f441120ce074f10d9ff
Carina <czsanbor@andrew.cmu.edu>
2024-02-15 09:56:34 -0500

    Create initial files

---
+++ include/search/planners/lpastar.hpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2024, Itamar Mishani
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Carnegie Mellon University nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/*!
+ * \file   lpastar.hpp
+ * \author Carina Sanborn (czsanbor@andrew.cmu.edu)
+ * \date   2/9/24
+*/
+
+#ifndef SEARCH_LPASTAR_HPP
+#define SEARCH_LPASTAR_HPP
+
+// standard includes
+#include <functional>
+// Standard includes
+#include <utility>
+#include <algorithm>
+// project includes
+#include <search/planners/best_first_search.hpp>
+
+namespace ims{
+
+    /// @class LPAStarParams class.
+    /// @brief The parameters for the LPAStar algorithm
+    struct LPAStarParams : public BestFirstSearchParams{
+
+        /// @brief Constructor
+        /// @param heuristic The heuristic function. Passing the default heuristic function will result in a uniform cost search
+        explicit LPAStarParams(BaseHeuristic* heuristic) : BestFirstSearchParams(heuristic) {}
+
+        /// @brief Destructor
+        ~LPAStarParams() override = default;
+
+    };
+
+    /// @class LPAStar class. Lifelong Planning A* algorithm
+    /// @brief A Lifelong Planning A* algorithm implementation.  LPA* is an incremental version of A*. 
+    /// It applies to finite graph search problems on known graphs whose edge costs increase or decrease over time (which can also be used to
+    /// model edges or vertices that are added or deleted).
+    class LPAStar : public BestFirstSearch{
+
+    private:
+
+        friend class AStar; friend class Dijkstra;
+        friend class ExperienceWAstar; friend class EAwAStarUniformCost;
+
+        /// @brief The search state.
+        struct SearchState: public ims::BestFirstSearch::SearchState{
+            /// @brief The heuristic value
+            double h {-1};
+        };
+
+        /// @brief The open list.
+        using OpenList = ::smpl::IntrusiveHeap<SearchState, SearchStateCompare>;
+        OpenList open_;
+
+        std::vector<SearchState*> states_;
+
+        /// @brief Get the state by id
+        /// @param state_id The id of the state
+        /// @return The state
+        /// @note Use this function only if you are sure that the state exists
+        auto getSearchState(int state_id) -> SearchState*;
+
+        /// @brief Get the state by id or create a new one if it does not exist
+        /// @param state_id The id of the state
+        /// @return The state
+        auto getOrCreateSearchState(int state_id) -> SearchState*;
+
+    public:
+        /// @brief Constructor
+        /// @param params The parameters
+        explicit LPAStar(const LPAStarParams &params);
+
+        /// @brief Destructor
+        ~LPAStar() override;
+
+        /// @brief Initialize the planner
+        /// @param action_space_ptr The action space
+        /// @param starts Vector of start states
+        /// @param goals Vector of goal states
+        void initializePlanner(const std::shared_ptr<ActionSpace>& action_space_ptr,
+                               const std::vector<StateType>& starts,
+                               const std::vector<StateType>& goals) override;
+
+        /// @brief Initialize the planner
+        /// @param action_space_ptr The action space
+        /// @param start The start state
+        /// @param goal The goal state
+        void initializePlanner(const std::shared_ptr<ActionSpace>& action_space_ptr,
+                               const StateType& start, const StateType& goal) override;
+
+        /// @brief plan a path
+        /// @param path The path
+        /// @return if the plan was successful or not
+        bool plan(std::vector<StateType> &path) override;
+
+        /// @brief Reset the planner
+        void resetPlanningData() override;
+
+        /// @brief Get all the search states.
+        /// @return The states.
+        auto getAllSearchStates() -> std::vector<SearchState*>;
+
+        
+    protected:
+
+        void setStateVals(int state_id, int parent_id, double cost) override;
+
+        void expand(int state_id) override;
+
+        void reconstructPath(std::vector<StateType>& path) override;
+
+        /// @brief Reconstruct the path and also get the transition costs.
+        /// @param path The path to be populated
+        /// @param costs The costs to be populated. Cost at index i is the cost of the transition from state i to state i+1. Thus, the cost at the goal state is zero as there is no transition from the goal state.
+        void reconstructPath(std::vector<StateType>& path, std::vector<double>& costs) override;
+
+        LPAStarParams params_;
+
+    };
+
+}
+
+
+#endif //SEARCH_LPASTAR_HPP
---
+++ src/planners/lpastar.cpp
@@ -0,0 +1,262 @@
+/*
+ * Copyright (C) 2024, Itamar Mishani
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Carnegie Mellon University nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/*!
+ * \file   lpastar.cpp
+ * \author Carina Sanborn (czsanbor@andrew.cmu.edu)
+ * \date   2/9/24
+*/
+
+#include <search/planners/lpastar.hpp>
+
+ims::LPAStar::LPAStar(const ims::LPAStarParams &params) : params_(params), BestFirstSearch(params) {}
+
+ims::LPAStar::~LPAStar() {
+    for (ims::LPAStar::SearchState *&state : states_) {
+        delete state;
+    }
+}
+
+void ims::LPAStar::initializePlanner(const std::shared_ptr<ActionSpace> &action_space_ptr,
+                                    const std::vector<StateType> &starts,
+                                    const std::vector<StateType> &goals) {
+    // space pointer
+    action_space_ptr_ = action_space_ptr;
+    // Clear both.
+    action_space_ptr_->resetPlanningData();
+    resetPlanningData();
+
+    if (goals.empty() || starts.empty()) {
+        throw std::runtime_error("Starts or goals are empty");
+    }
+
+    if (goals.size() > 1) {
+        throw std::runtime_error("Currently, only one goal is supported");
+    }
+    // check if goal is valid
+    if (!action_space_ptr_->isStateValid(goals[0])){
+        throw std::runtime_error("Goal state is not valid");
+    }
+    int goal_ind_ = action_space_ptr_->getOrCreateRobotState(goals[0]);
+    ims::LPAStar::SearchState *goal_ = getOrCreateSearchState(goal_ind_);
+    goals_.push_back(goal_ind_);
+
+    // Evaluate the goal state
+    goal_->parent_id = PARENT_TYPE(GOAL);
+    heuristic_->setGoal(const_cast<StateType &>(goals[0]));
+    goal_->h = 0;
+
+    for (const StateType &start : starts) {
+        // check if start is valid
+        if (!action_space_ptr_->isStateValid(start)){
+            throw std::runtime_error("Start state is not valid");
+        }
+        // Evaluate the start state
+        int start_ind_ = action_space_ptr_->getOrCreateRobotState(start);
+        auto start_ = getOrCreateSearchState(start_ind_);
+        start_->parent_id = PARENT_TYPE(START);
+        heuristic_->setStart(const_cast<StateType &>(start));
+        start_->g = 0;
+        start_->h = computeHeuristic(start_ind_);
+        start_->f = start_->g + start_->h;
+        open_.push(start_);
+        start_->setOpen();
+    }
+}
+
+void ims::LPAStar::initializePlanner(const std::shared_ptr<ActionSpace>& action_space_ptr,
+                                   const StateType& start, const StateType& goal) {
+    // Space pointer.
+    action_space_ptr_ = action_space_ptr;
+
+    // Clear both.
+    action_space_ptr_->resetPlanningData();
+    resetPlanningData();
+
+    // check if start is valid
+    if (!action_space_ptr_->isStateValid(start)){
+        throw std::runtime_error("Start state is not valid");
+    }
+    // check if goal is valid
+    if (!action_space_ptr_->isStateValid(goal)){
+        throw std::runtime_error("Goal state is not valid");
+    }
+    int start_ind_ = action_space_ptr_->getOrCreateRobotState(start);
+    auto start_ = getOrCreateSearchState(start_ind_);
+
+    int goal_ind_ = action_space_ptr_->getOrCreateRobotState(goal);
+    auto goal_ = getOrCreateSearchState(goal_ind_);
+    goals_.push_back(goal_ind_);
+
+    start_->parent_id = PARENT_TYPE(START);
+    heuristic_->setStart(const_cast<StateType &>(start));
+    // Evaluate the goal state
+    goal_->parent_id = PARENT_TYPE(GOAL);
+    heuristic_->setGoal(const_cast<StateType &>(goal));
+    goal_->h = 0;
+    // Evaluate the start state
+    start_->g = 0;
+    start_->h = computeHeuristic(start_ind_);
+    start_->f = start_->g + start_->h;
+    start_->setOpen();
+
+    open_.push(start_);
+}
+
+ims::LPAStar::SearchState *ims::LPAStar::getSearchState(int state_id) {
+    assert(state_id < states_.size() && state_id >= 0);
+    return states_[state_id];
+}
+
+ims::LPAStar::SearchState *ims::LPAStar::getOrCreateSearchState(int state_id){
+    if (state_id >= states_.size()){
+        states_.resize(state_id + 1, nullptr);
+    }
+    if (states_[state_id] == nullptr){
+        assert(state_id < states_.size() && state_id >= 0);
+        states_[state_id] = new SearchState;
+        states_[state_id]->state_id = state_id;
+    }
+    return states_[state_id];
+}
+
+bool ims::LPAStar::plan(std::vector<StateType>& path) {
+    startTimer();
+    int iter {0};
+    while (!open_.empty() && !isTimeOut()){
+        // report progress every 1000 iterations
+        if (iter % 100000 == 0 && params_.verbose){
+            std::cout << "Iter: " << iter << " open size: " << open_.size() << std::endl;
+        }
+        ims::LPAStar::SearchState *state  = open_.min();
+        open_.pop();
+        state->setClosed();
+        if (isGoalState(state->state_id)){
+            goal_ = state->state_id;
+            getTimeFromStart(stats_.time);
+            reconstructPath(path, stats_.transition_costs);
+            stats_.cost = state->g;
+            stats_.path_length = (int)path.size();
+            stats_.num_generated = (int)action_space_ptr_->states_.size();
+            return true;
+        }
+        expand(state->state_id);
+        ++iter;
+    }
+    getTimeFromStart(stats_.time);
+    return false;
+}
+
+void ims::LPAStar::expand(int state_id){
+
+    auto state_ = getSearchState(state_id);
+    std::vector<int> successors;
+    std::vector<double> costs;
+    action_space_ptr_->getSuccessors(state_->state_id, successors, costs);
+    for (size_t i {0} ; i < successors.size() ; ++i){
+        int successor_id = successors[i];
+        double cost = costs[i];
+        auto successor = getOrCreateSearchState(successor_id);
+        if (successor->in_closed){
+            continue;
+        }
+        if (isGoalState(successor_id) && params_.verbose ){
+            std::cout << "Added Goal to open list" << std::endl;
+        }
+        if (successor->in_open){
+            if (successor->g > state_->g + cost){
+                successor->parent_id = state_->state_id;
+                successor->g = state_->g + cost;
+                successor->f = successor->g + successor->h;
+                open_.update(successor);
+            }
+        } else {
+            setStateVals(successor->state_id, state_->state_id, cost);
+            open_.push(successor);
+            successor->setOpen();
+        }
+    }
+    stats_.num_expanded++;
+}
+
+void ims::LPAStar::setStateVals(int state_id, int parent_id, double cost)
+{
+    auto state_ = getSearchState(state_id);
+    auto parent = getSearchState(parent_id);
+    state_->parent_id = parent_id;
+    state_->g = parent->g + cost;
+    state_->h = computeHeuristic(state_id);
+    state_->f = state_->g + state_->h;
+}
+
+
+void ims::LPAStar::reconstructPath(std::vector<StateType>& path, std::vector<double>& costs) {
+    path.clear();
+    costs.clear();
+
+    costs.push_back(0); // The goal state gets a transition cost of 0.
+    SearchState* state_ = getSearchState(goal_);
+    while (state_->parent_id != -1){
+        path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+        
+        // Get the transition cost. This is the difference between the g values of the current state and its parent.
+        double transition_cost = state_->g - getSearchState(state_->parent_id)->g;
+        costs.push_back(transition_cost);
+
+        state_ = getSearchState(state_->parent_id);
+    }
+    path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+
+    std::reverse(path.begin(), path.end());
+    std::reverse(costs.begin(), costs.end());   
+}
+
+void ims::LPAStar::reconstructPath(std::vector<StateType>& path) {
+    SearchState* state_ = getSearchState(goal_);
+    while (state_->parent_id != -1){
+        path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+        state_ = getSearchState(state_->parent_id);
+    }
+    path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+    std::reverse(path.begin(), path.end());
+}
+
+void ims::LPAStar::resetPlanningData(){
+    for (ims::LPAStar::SearchState *state_ : states_){
+        delete state_;
+    }
+    states_.clear();
+    open_.clear();
+    goals_.clear();
+    goal_ = -1;
+    stats_ = PlannerStats();
+}
+
+std::vector<ims::LPAStar::SearchState*> ims::LPAStar::getAllSearchStates() {
+    return states_;
+}
