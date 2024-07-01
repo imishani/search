94eb0654e0bf26a08637c694ceba42ab63cfa922
yoraish <yorai.shaoul@gmail.com>
2024-07-01 17:12:07 -0400

    feat(subcost_actions_pace): getSuccessors deprecated.

--- include/search/action_space/mixin/action_space_subcost_mixin.hpp
+++ include/search/action_space/mixin/action_space_subcost_mixin.hpp
@@ -62,6 +62,7 @@ public:
                             std::vector<std::vector<double>> & edges_transition_costs,
                             std::vector<std::vector<double>> & edge_transition_subcosts) = 0;
 
+    [[deprecated("Use getSuccessorEdges instead.")]]
     bool getSuccessors(int curr_state_ind,
                             std::vector<int> & successors,
                             std::vector<double> & costs,
@@ -71,9 +72,16 @@ public:
         std::vector<std::vector<double>> edge_transition_subcosts;
         getSuccessorEdges(curr_state_ind, edges_state_ids, edges_transition_costs, edge_transition_subcosts);
         for (size_t i = 0; i < edges_state_ids.size(); i++) {
+            // Check that the edges have only two elements (the parent and child states). Otherwise, abort with a message.
+            if (edges_state_ids[i].size() != 2){
+                std::cout << RED << "getSuccessors: The edges_state_ids[i] should have only two elements (the parent and child states). Instead, it has " << edges_state_ids[i].size() << " elements." << RESET << std::endl;
+                std::cout << RED << "Edge state ids: " << edges_state_ids[i] << RESET << std::endl;
+                std::cout << RED << "GetSuccessors would have returned the edge state ids: [" << edges_state_ids[i].front() << ", " << edges_state_ids[i].back() << "], which would lose information." << RESET << std::endl;
+                throw std::runtime_error("getSuccessors: The edges_state_ids should have only two elements (the parent and child states).");
+            }
             successors.push_back(edges_state_ids[i].back());
-            costs.push_back(std::accumulate(edges_transition_costs[i].begin(), edges_transition_costs[i].end(), 0.0));
-            subcosts.push_back(std::accumulate(edge_transition_subcosts[i].begin(), edge_transition_subcosts[i].end(), 0.0));
+            costs.push_back(vectorSum(edges_transition_costs[i]));
+            subcosts.push_back(vectorSum(edge_transition_subcosts[i]));
         }
         return true;
     }
