a4cddf1ecb6943d75348b157b2716f12d76bd5e8
Hanlan Yang <yanghanlan666@gmail.com>
2024-07-15 16:05:03 -0400

    feat(parallel): formatting according to google style

--- src/planners/parallel_search/parallel_search.cpp
+++ src/planners/parallel_search/parallel_search.cpp
@@ -34,108 +34,106 @@
 
 #include <search/planners/parallel_search/parallel_search.hpp>
 
-ims::ParallelSearch::ParallelSearch(const ParallelSearchParams &params) : Planner(params), params_(params) {
-    heuristic_ = params.heuristic_;
-    std::vector<LockType> lock_vec(params.num_threads_-1);
-    locks_.swap(lock_vec);
+ims::ParallelSearch::ParallelSearch(const ParallelSearchParams& params) : Planner(params), params_(params) {
+  heuristic_ = params.heuristic_;
+  std::vector<LockType> lock_vec(params.num_threads_ - 1);
+  locks_.swap(lock_vec);
 }
 
 ims::ParallelSearch::~ParallelSearch() {
-    for (auto &state : states_) {
-        delete state;
-    }
+  for (auto& state : states_) {
+    delete state;
+  }
 }
 
-auto ims::ParallelSearch::getSearchState(int state_id) -> ims::ParallelSearch::SearchState*{
+auto ims::ParallelSearch::getSearchState(int state_id) -> ims::ParallelSearch::SearchState* {
+  assert(state_id < states_.size() && state_id >= 0);
+  return states_[state_id];
+}
+
+auto ims::ParallelSearch::getOrCreateSearchState(int state_id) -> ims::ParallelSearch::SearchState* {
+  if (state_id >= states_.size()) {
+    states_.resize(state_id + 1, nullptr);
+  }
+  if (states_[state_id] == nullptr) {
     assert(state_id < states_.size() && state_id >= 0);
-    return states_[state_id];
-}
-
-auto ims::ParallelSearch::getOrCreateSearchState(int state_id) -> ims::ParallelSearch::SearchState * {
-    if (state_id >= states_.size()){
-        states_.resize(state_id + 1, nullptr);
-    }
-    if (states_[state_id] == nullptr){
-        assert(state_id < states_.size() && state_id >= 0);
-        states_[state_id] = new SearchState;
-        states_[state_id]->state_id = state_id;
-    }
-    return states_[state_id];
+    states_[state_id] = new SearchState;
+    states_[state_id]->state_id = state_id;
+  }
+  return states_[state_id];
 }
 
 double ims::ParallelSearch::computeHeuristic(int state_id) {
-    double dist;
-    auto s = action_space_ptr_->getRobotState(state_id);
-    if (!heuristic_->getHeuristic(s->state, dist))
-        throw std::runtime_error("Heuristic function failed");
-    else
-        return dist;
+  double dist;
+  auto s = action_space_ptr_->getRobotState(state_id);
+  if (!heuristic_->getHeuristic(s->state, dist))
+    throw std::runtime_error("Heuristic function failed");
+  else
+    return dist;
 }
 
 double ims::ParallelSearch::computeHeuristic(int s1_id, int s2_id) {
-    double dist;
-    auto s1 = action_space_ptr_->getRobotState(s1_id);
-    auto s2 = action_space_ptr_->getRobotState(s2_id);
-    if (!heuristic_->getHeuristic(s1->state, s2->state, dist))
-        throw std::runtime_error("Heuristic function failed");
-    else
-        return dist;
+  double dist;
+  auto s1 = action_space_ptr_->getRobotState(s1_id);
+  auto s2 = action_space_ptr_->getRobotState(s2_id);
+  if (!heuristic_->getHeuristic(s1->state, s2->state, dist))
+    throw std::runtime_error("Heuristic function failed");
+  else
+    return dist;
 }
 
 void ims::ParallelSearch::reconstructPath(std::vector<StateType>& path) {
-    path.clear();
+  path.clear();
 
-    SearchState* state = getSearchState(goal_);
-    while (state->parent_id != -1){
-        path.push_back(action_space_ptr_->getRobotState(state->state_id)->state);
-        state = getSearchState(state->parent_id);
-    }
+  SearchState* state = getSearchState(goal_);
+  while (state->parent_id != -1) {
     path.push_back(action_space_ptr_->getRobotState(state->state_id)->state);
-    std::reverse(path.begin(), path.end());
+    state = getSearchState(state->parent_id);
+  }
+  path.push_back(action_space_ptr_->getRobotState(state->state_id)->state);
+  std::reverse(path.begin(), path.end());
 }
 
 void ims::ParallelSearch::reconstructPath(std::vector<StateType>& path, std::vector<double>& costs) {
-    path.clear();
-    costs.clear();
+  path.clear();
+  costs.clear();
 
-    costs.push_back(0); // The goal state gets a transition cost of 0.
-    SearchState* state_ = getSearchState(goal_);
-    while (state_->parent_id != -1){
-        path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
-        
-        // Get the transition cost. This is the difference between the g values of the current state and its parent.
-        double transition_cost = state_->g - getSearchState(state_->parent_id)->g;
-        costs.push_back(transition_cost);
-
-        state_ = getSearchState(state_->parent_id);
-    }
+  costs.push_back(0);  // The goal state gets a transition cost of 0.
+  SearchState* state_ = getSearchState(goal_);
+  while (state_->parent_id != -1) {
     path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
 
-    std::reverse(path.begin(), path.end());
-    std::reverse(costs.begin(), costs.end());   
-}
+    // Get the transition cost. This is the difference between the g values of the current state and its parent.
+    double transition_cost = state_->g - getSearchState(state_->parent_id)->g;
+    costs.push_back(transition_cost);
 
+    state_ = getSearchState(state_->parent_id);
+  }
+  path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+
+  std::reverse(path.begin(), path.end());
+  std::reverse(costs.begin(), costs.end());
+}
 
 bool ims::ParallelSearch::isGoalState(int s_id) {
-    if (std::any_of(goals_.begin(), goals_.end(), [&s_id](int goal_ind) {return s_id == goal_ind;})){
-        return true;
-    }
+  if (std::any_of(goals_.begin(), goals_.end(), [&s_id](int goal_ind) { return s_id == goal_ind; })) {
+    return true;
+  }
 
-    // Also ask the action space if this state id is a goal state. Sometimes, states need to be determined as goal in real time.
-    // This is EXTREMELY bug prone. Commenting this out for now until we have a proper GoalCondition object.
-    // return action_space_ptr_->isGoalState(s_id, goals_);
-    return false;
+  // Also ask the action space if this state id is a goal state. Sometimes, states need to be determined as goal in real time.
+  // This is EXTREMELY bug prone. Commenting this out for now until we have a proper GoalCondition object.
+  // return action_space_ptr_->isGoalState(s_id, goals_);
+  return false;
 }
 
-
 void ims::ParallelSearch::resetPlanningData() {
-    for (auto state : states_){
-        delete state;
-    }
-    states_ = std::vector<SearchState*>();
-    // open_.clear();
+  for (auto state : states_) {
+    delete state;
+  }
+  states_ = std::vector<SearchState*>();
+  // open_.clear();
 
-    goals_.clear();
-    goal_ = -1;
-    stats_ = ParallelSearchPlannerStats();
+  goals_.clear();
+  goal_ = -1;
+  stats_ = ParallelSearchPlannerStats();
 }
