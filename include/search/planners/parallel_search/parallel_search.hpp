95e072ae12f74f1d99788504bee24f96f80ae312
Hanlan Yang <yanghanlan666@gmail.com>
2024-07-17 16:44:12 -0400

    feat(parallel): merge parallel_search.cpp into hpp

--- include/search/planners/parallel_search/parallel_search.hpp
+++ include/search/planners/parallel_search/parallel_search.hpp
@@ -70,8 +70,9 @@ struct ParallelSearchPlannerStats : public PlannerStats {
     std::vector<int> num_jobs_per_thread;  // The number of jobs per thread
 };
 
-/// @class ParallelSearch Abstract class.
-/// @brief A Abstract class for parallel search algorithm that utilize multi-threads to find the optimal path
+/// @class ParallelSearch Base class.
+/// @brief A base class for parallel search algorithm that utilize multi-threads to find the optimal path
+/// @note This class cannot run by itself as it is lacking implementation in essential functions (plan(),etc.).
 class ParallelSearch : public Planner {
    protected:
     /*ABSTRACT LEVEL/STRUCT DEFINITION*/
@@ -176,44 +177,127 @@ class ParallelSearch : public Planner {
     std::atomic<bool> recheck_flag_{true};
 
     /*FUNCTIONS*/
+
+    /**Not Implemented**/
+
+    /// @brief independency check to prevent re-expansion/re-evaluation
+    virtual bool independentCheck(int id, const boost::any& popped_vec) = 0;
+
+    /**Implemented**/
+
     /// @brief Get the state by id
     /// @param state_id The id of the state
     /// @return The state
     /// @note Use this function only if you are sure that the state exists.
-    auto getSearchState(int state_id) -> std::shared_ptr<SearchState>;
+    auto getSearchState(int state_id) -> std::shared_ptr<SearchState> {
+        assert(state_id < states_.size() && state_id >= 0);
+        return states_[state_id];
+    }
 
     /// @brief Get the state by id or create a new one if it does not exist. If a search state does not exist yet and a new one is created, it's ID will be set, and all other member fields will initialize to default values.
     /// @param state_id The id of the state
     /// @return The state
-    auto getOrCreateSearchState(int state_id) -> std::shared_ptr<SearchState>;
+    auto getOrCreateSearchState(int state_id) -> std::shared_ptr<SearchState> {
+        if (state_id >= states_.size()) {
+            states_.resize(state_id + 1, nullptr);
+        }
+        if (states_[state_id] == nullptr) {
+            assert(state_id < states_.size() && state_id >= 0);
+            states_[state_id] = std::make_shared<SearchState>();
+            states_[state_id]->state_id = state_id;
+        }
+        return states_[state_id];
+    }
 
     /// @brief Compute the heuristic value of from state s to the goal state
     /// @param s The state
-    double computeHeuristic(int state_id);
+    double computeHeuristic(int state_id) {
+        double dist;
+        auto s = action_space_ptr_->getRobotState(state_id);
+        if (!heuristic_->getHeuristic(s->state, dist))
+            throw std::runtime_error("Heuristic function failed");
+        else
+            return dist;
+    }
 
     /// @brief Compute the heuristic value from state s1 to state s2
     /// @param s1 The state
     /// @param s2 The state
-    double computeHeuristic(int s1_id, int s2_id);
+    double computeHeuristic(int s1_id, int s2_id) {
+        double dist;
+        auto s1 = action_space_ptr_->getRobotState(s1_id);
+        auto s2 = action_space_ptr_->getRobotState(s2_id);
+        if (!heuristic_->getHeuristic(s1->state, s2->state, dist))
+            throw std::runtime_error("Heuristic function failed");
+        else
+            return dist;
+    }
 
     /// @brief Reconstruct the path by backtracking
     /// @param path The reference of vector of states to be populated
-    void reconstructPath(std::vector<StateType>& path) override;
+    void reconstructPath(std::vector<StateType>& path) override {
+        path.clear();
+
+        std::shared_ptr<SearchState> state = getSearchState(goal_);
+        while (state->parent_id != -1) {
+            path.push_back(action_space_ptr_->getRobotState(state->state_id)->state);
+            state = getSearchState(state->parent_id);
+        }
+        path.push_back(action_space_ptr_->getRobotState(state->state_id)->state);
+        std::reverse(path.begin(), path.end());
+    }
 
     /// @brief Reconstruct the path by backtracking
     /// @param path The reference of vector of states to be populated
     /// @param costs The reference of vector of costs to be populated (edge costs)
-    void reconstructPath(std::vector<StateType>& path, std::vector<double>& costs) override;
+    void reconstructPath(std::vector<StateType>& path, std::vector<double>& costs) override {
+        path.clear();
+        costs.clear();
 
-    bool isGoalState(int state_id) override;
+        costs.push_back(0);  // The goal state gets a transition cost of 0.
+        std::shared_ptr<SearchState> state_ = getSearchState(goal_);
+        while (state_->parent_id != -1) {
+            path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+
+            // Get the transition cost. This is the difference between the g values of the current state and its parent.
+            double transition_cost = state_->g - getSearchState(state_->parent_id)->g;
+            costs.push_back(transition_cost);
+
+            state_ = getSearchState(state_->parent_id);
+        }
+        path.push_back(action_space_ptr_->getRobotState(state_->state_id)->state);
+
+        std::reverse(path.begin(), path.end());
+        std::reverse(costs.begin(), costs.end());
+    }
+
+    inline bool isGoalState(int state_id) override {
+        if (std::any_of(goals_.begin(), goals_.end(), [&state_id](int goal_ind) { return state_id == goal_ind; })) {
+            return true;
+        }
+        // Also ask the action space if this state id is a goal state. Sometimes, states need to be determined as goal in real time.
+        // This is EXTREMELY bug prone. Commenting this out for now until we have a proper GoalCondition object.
+        // return action_space_ptr_->isGoalState(s_id, goals_);
+        return false;
+    }
 
     /// @brief Throw exception if the planner is not initialized correctly
     /// @note I am adding this function since I am not entirely comfortable exposing the
     /// initializePlanner() to user. Adding to prevent double plan() calls.
-    virtual void initializeCheck() const;
-
-    /// @brief independency check to prevent re-expansion/re-evaluation
-    virtual bool independentCheck(int id, const boost::any& popped_vec) = 0;
+    void initializeCheck() const {
+        if (action_space_ptr_ == nullptr) {
+            throw std::runtime_error("Action space is not initialized");
+        }
+        if (heuristic_ == nullptr) {
+            throw std::runtime_error("Heuristic is not initialized");
+        }
+        if (goals_.empty() || goal_ == -1) {
+            throw std::runtime_error("Goals are not initialized");
+        }
+        if (open_->empty()) {
+            throw std::runtime_error("Open list is empty, make sure to initialize the planner before run");
+        }
+    }
 
     /// @brief check if a thread is terminated using std::future
     template <typename T>
@@ -228,15 +312,41 @@ class ParallelSearch : public Planner {
     }
 
     /// @brief helper function to join all the spawned threads
-    void cleanUp();
+    void joinThread() {
+        // Notify all worker threads to stop.
+        for (int thread_id{0}; thread_id < params_.num_threads_; ++thread_id) {
+            std::unique_lock<LockType> locker(lock_vec_[thread_id]);
+            work_status_[thread_id] = true;
+            locker.unlock();
+            cv_vec_[thread_id].notify_one();
+        }
+
+        stats_.num_threads_spawned = work_futures_.size() + 1;
+
+        bool all_threads_exit = false;
+        while (!all_threads_exit) {
+            all_threads_exit = true;
+            for (auto& fut : work_futures_) {
+                if (!isFutureReady(fut)) {
+                    all_threads_exit = false;
+                    break;
+                }
+            }
+        }
+        work_futures_.clear();
+    }
 
    public:
+    /**Not Implemented**/
+    
     /// @brief Constructor
     /// @param params The parameters
-    explicit ParallelSearch(const ParallelSearchParams& params);
+    explicit ParallelSearch(const ParallelSearchParams& params) : Planner(params), params_(params) {
+        heuristic_ = params.heuristic_;        
+    }
 
     /// @brief Destructor
-    ~ParallelSearch() override;
+    ~ParallelSearch() override = default;
 
     /// @brief Initialize the planner
     /// @param action_space_ptr The action space
@@ -258,9 +368,34 @@ class ParallelSearch : public Planner {
     /// @return if the plan was successful or not
     virtual bool plan(std::vector<StateType>& path) = 0;
 
-    virtual void resetPlanningData() override;
+    /**Implemented**/
+
+    virtual void resetPlanningData() override {
+        // First dealing with raw pointers data structure
+        open_->clear();
+
+        states_.clear();
+        states_ = std::vector<std::shared_ptr<SearchState>>();
+
+        goals_.clear();
+        goal_ = -1;
+        stats_ = ParallelSearchPlannerStats();
+        stats_.num_jobs_per_thread.resize(params_.num_threads_, 0);
+
+        work_in_progress_->resize(params_.num_threads_ - 1, NULL);
+        std::vector<std::condition_variable> cv_vec(params_.num_threads_ - 1);
+        cv_vec_.swap(cv_vec);
+        std::vector<LockType> lock_vec(params_.num_threads_ - 1);
+        lock_vec_.swap(lock_vec);
+        work_futures_.clear();
+        work_status_.resize(params_.num_threads_ - 1, false);
+        terminate_ = false;
+        plan_found_ = false;
+        recheck_flag_ = true;
+    }
 
     inline ParallelSearchPlannerStats reportStats() { return stats_; }
+
     inline ParallelSearchPlannerStats getStats() { return stats_; }
 
 };  // class Parallel Search
